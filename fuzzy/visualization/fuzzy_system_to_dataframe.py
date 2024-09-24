04c0e7a882804ac1815153a9b1cac5f99e26b487
Leite <tiagoleite2002@outlook.com>
2024-09-24 13:15:05 +0100

    Corrected triangular bug

--- fuzzy/visualization/fuzzy_system_to_dataframe.py
+++ fuzzy/visualization/fuzzy_system_to_dataframe.py
@@ -22,23 +22,45 @@ def fuzzy_system_to_dataframe(FS: FuzzySystem) -> pd.DataFrame:
             # Fuzzy set is Triangular
             if isinstance(fs._funpointer, Triangular_MF):
 
-                vis['x'].append(fs._funpointer._a)
-                vis['y'].append(0)
+                if fs._funpointer._a == fs._funpointer._b:
+                    vis['x'].append(fs._funpointer._a)
+                    vis['y'].append(1)
 
-                vis['x'].append(fs._funpointer._b)
-                vis['y'].append(1.0)
+                    vis['x'].append(fs._funpointer._c)
+                    vis['y'].append(0)
 
-                vis['x'].append(fs._funpointer._c)
-                vis['y'].append(0)
+                    for _ in range(2):
+                        vis['term'].append(fs._term)
+                        vis['linguistic_var'].append(key)
 
-                vis['term'].append(fs._term)
-                vis['linguistic_var'].append(key)
+                elif fs._funpointer._b == fs._funpointer._c:
+                    vis['x'].append(fs._funpointer._a)
+                    vis['y'].append(0)
 
-                vis['term'].append(fs._term)
-                vis['linguistic_var'].append(key)
+                    vis['x'].append(fs._funpointer._c)
+                    vis['y'].append(1)
 
-                vis['term'].append(fs._term)
-                vis['linguistic_var'].append(key)
+                    for _ in range(2):
+                        vis['term'].append(fs._term)
+                        vis['linguistic_var'].append(key)
+                else:
+                    vis['x'].append(fs._funpointer._a)
+                    vis['y'].append(0)
+
+                    vis['x'].append(fs._funpointer._b)
+                    vis['y'].append(1.0)
+
+                    vis['x'].append(fs._funpointer._c)
+                    vis['y'].append(0)
+
+                    vis['term'].append(fs._term)
+                    vis['linguistic_var'].append(key)
+
+                    vis['term'].append(fs._term)
+                    vis['linguistic_var'].append(key)
+
+                    vis['term'].append(fs._term)
+                    vis['linguistic_var'].append(key)
 
             # Fuzzy set is Gaussian
             if isinstance(fs._funpointer, Gaussian_MF):
