509317ecc4e3a7178533361ca9679f1c981f6b94
Leite <tiagoleite2002@outlook.com>
2024-10-01 20:05:32 +0100

    Added example to get data from NN

--- TestMe.py
+++ TestMe.py
@@ -1,8 +1,15 @@
-
+from nn.models.simple_lightning import Net
 # parse input file argument
 
 # load FS model
 
 # load NN model
+model = Net.load_from_checkpoint("./nn/models/lightning_logs/simple_model/version_3/epoch=99-step=600.ckpt")
+
+# disable randomness, dropout, etc...
+model.eval()
+
+# predict with the model
+y_hat = model(x)
 
 # Output TestResult.csv file
