1a07a5cb8b8f6a3796e4888ec8b4d4c6ff7b4c5e
Leite <tiagoleite2002@outlook.com>
2024-10-05 14:13:48 +0100

    generate_data.py works

---
+++ fuzzy/generate_data.py
@@ -0,0 +1,30 @@
+from fuzzy.models.mamdani_hparams import create_fuzzy_system
+from fuzzy.models.mamdani_best import FS
+import numpy as np
+import pandas as pd
+
+
+
+sample_distribution = np.random.uniform
+
+N_SAMPLES = 100
+df = pd.DataFrame({
+    'MemoryUsage': sample_distribution(size=N_SAMPLES),
+    'ProcessorLoad': sample_distribution(size=N_SAMPLES),
+    'InpNetThroughput': sample_distribution(size=N_SAMPLES),
+    'OutNetThroughput': sample_distribution(size=N_SAMPLES),
+    'OutBandwidth': sample_distribution(size=N_SAMPLES),
+    'Latency': sample_distribution(size=N_SAMPLES),
+    'V_MemoryUsage': sample_distribution(size=N_SAMPLES),
+    'V_ProcessorLoad': sample_distribution(size=N_SAMPLES),
+    'V_InpNetThroughput': sample_distribution(size=N_SAMPLES),
+    'V_OutNetThroughput': sample_distribution(size=N_SAMPLES),
+    'V_OutBandwidth': sample_distribution(size=N_SAMPLES),
+    'V_Latency': sample_distribution(size=N_SAMPLES),
+    #'CLPVariation': sample_distribution(low=-1, high=1, size=N_SAMPLES),
+})
+
+clp = FS.predict(df)
+df['CLPVariation'] = clp
+print(df[['MemoryUsage', 'ProcessorLoad', 'Latency', 'CLPVariation']].head())
+df.to_csv(f'../gen_input/{sample_distribution.__name__}{N_SAMPLES}.csv', index=False)
