7277ccfcef2a71aebc72b84515ab7fc2985a1daf
Leite <tiagoleite2002@outlook.com>
2024-10-05 15:07:07 +0100

    Added progress bar functionality

--- fuzzy/fuzzy_system_wrapper.py
+++ fuzzy/fuzzy_system_wrapper.py
@@ -4,24 +4,33 @@ import numpy as np
 
 
 class FuzzySystemWrapper(FuzzySystem):
-    def predict(self, df: pd.DataFrame) -> np.array:
-        fs_results = []
-        # Maybe in future create a predict function common to all FS that receives df
-        for i, row in df.iterrows():
-            self.set_variable('MemoryUsage', row['MemoryUsage'])
-            self.set_variable('ProcessorLoad', row['ProcessorLoad'])
-            self.set_variable('InpNetThroughput', row['InpNetThroughput'])
-            self.set_variable('OutNetThroughput', row['OutNetThroughput'])
-            self.set_variable('OutBandwidth', row['OutBandwidth'])
-            self.set_variable('Latency', row['Latency'])
-            self.set_variable('V_MemoryUsage', row['V_MemoryUsage'])
-            self.set_variable('V_ProcessorLoad', row['V_ProcessorLoad'])
-            self.set_variable('V_InpNetThroughput', row['V_InpNetThroughput'])
-            self.set_variable('V_OutNetThroughput', row['V_OutNetThroughput'])
-            self.set_variable('V_OutBandwidth', row['V_OutBandwidth'])
-            self.set_variable('V_Latency', row['V_Latency'])
+    def _set_row(self, row):
+        self.set_variable('MemoryUsage', row['MemoryUsage'])
+        self.set_variable('ProcessorLoad', row['ProcessorLoad'])
+        self.set_variable('InpNetThroughput', row['InpNetThroughput'])
+        self.set_variable('OutNetThroughput', row['OutNetThroughput'])
+        self.set_variable('OutBandwidth', row['OutBandwidth'])
+        self.set_variable('Latency', row['Latency'])
+        self.set_variable('V_MemoryUsage', row['V_MemoryUsage'])
+        self.set_variable('V_ProcessorLoad', row['V_ProcessorLoad'])
+        self.set_variable('V_InpNetThroughput', row['V_InpNetThroughput'])
+        self.set_variable('V_OutNetThroughput', row['V_OutNetThroughput'])
+        self.set_variable('V_OutBandwidth', row['V_OutBandwidth'])
+        self.set_variable('V_Latency', row['V_Latency'])
 
-            SystemLoad = max(row['MemoryUsage'], row['ProcessorLoad'])
-            self.set_variable("SystemLoad", SystemLoad)
-            fs_results.append(self.inference()['CLP'])
+        SystemLoad = max(row['MemoryUsage'], row['ProcessorLoad'])
+        self.set_variable("SystemLoad", SystemLoad)
+
+    def predict(self, df: pd.DataFrame, show_bar=False) -> np.array:
+        fs_results = []
+        if not show_bar:
+            for i, row in df.iterrows():
+                self._set_row(row)
+                fs_results.append(self.inference()['CLP'])
+        else:
+            from tqdm import tqdm
+
+            for i, row in tqdm(df.iterrows(), total=df.shape[0]):
+                self._set_row(row)
+                fs_results.append(self.inference()['CLP'])
         return np.array(fs_results)
