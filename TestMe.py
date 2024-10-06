29e096d3557be8701c88d96242f136a437157094
Leite <tiagoleite2002@outlook.com>
2024-10-06 15:25:14 +0100

    Works for NN and FS regression

--- TestMe.py
+++ TestMe.py
@@ -7,15 +7,21 @@ import pickle
 from fuzzy.models.mamdani_best import FS
 
 
-SHOW_LATEX = True
+def mse(y_true, y_pred):
+    return ((y_true - y_pred)**2)/len(y_true)
+
+
+SHOW_LATEX = False
+SHOW_BAR_PLOT = True
 
 parser = argparse.ArgumentParser(
                     prog='TestMe -> Fuzzy & Neural Network CInt',
                     description='Tests a Fuzzy Inference System and a Neural Network on a given .csv file',
                     epilog='Thank you for choosing us!')
 
-parser.add_argument('filename', required=True)
+parser.add_argument('filename', type=str, help="Filename of the .csv file")
 args = parser.parse_args()
+
 
 # parse input file argument
 df = pd.read_csv(args.filename)
@@ -23,33 +29,44 @@ df = pd.read_csv(args.filename)
 required_columns = ('MemoryUsage', 'ProcessorLoad', 'InpNetThroughput', 'OutNetThroughput', 'OutBandwidth', 'Latency',
                     'V_MemoryUsage', 'V_ProcessorLoad', 'V_InpNetThroughput', 'V_OutNetThroughput', 'V_OutBandwidth', 'V_Latency')
 
-if not all(e in required_columns for e in df.columns):
+if not all(r in df.columns for r in required_columns):
     raise RuntimeError('Required columns are missing')
 
+# only use the required columns
+#df = df[list(required_columns)]  # this is required because NN model needs 12 features as input
 
 # test FS model
-fs_results = FS.predict(df)
+fs_results = FS.predict(df[list(required_columns)])
 
 
 # load NN model
-model = Net.load_from_checkpoint("./final_models/nn.ckpt")
+model = Net.load_from_checkpoint("nn/final_model/simple-epoch=85-val_loss=0.02.ckpt")
 
 # disable randomness, dropout, etc...
 model.eval()
 
 # predict with the model
-nn_results = model(torch.Tensor(df.values)).detach().numpy()
+nn_results = model(torch.Tensor(df[list(required_columns)].values)).detach().numpy().flatten()
 
-# TODO add NN Classification {increase, decrease, maintain}
-
-# TODO? add NN classification groundtruth
-
-# Output TestResult.csv file
-df = pd.DataFrame({
+df_results = {
     'CLPVariation_FS': fs_results,
     'CLPVariation_NN': nn_results,
-})
-df.to_csv('TestResult.csv', index=False)
-print(df.head(10))
+    # TODO add NN Classification {increase, decrease, maintain}
+
+    # TODO? add NN classification groundtruth
+}
+
+if 'CLPVariation' in df.columns:
+    df_results['MSE_FS'] = mse(df['CLPVariation'], fs_results)
+    df_results['MSE_NN'] = mse(df['CLPVariation'], nn_results)
+
+# Output TestResult.csv file
+df_result = pd.DataFrame(df_results)
+df_result.to_csv('TestResult.csv', index=False)
+print(df_result.head(10))
 if SHOW_LATEX:
-    print(df.to_latex(index=False, float_format='%.3f'))
+    print(df_result.to_latex(index=False, float_format='%.3f'))
+
+if SHOW_BAR_PLOT:
+    from fuzzy.visualization import plot_model_scores
+    plot_model_scores(df_result[['MSE_FS', 'MSE_NN']])
